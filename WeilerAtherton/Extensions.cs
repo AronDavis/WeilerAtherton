4ef9d77fe29489db5e8362559d49640b62d8c096
Aron Davis <arontd@gmail.com>
2015-04-17 10:38:13 -0400

    Added Extensions class to add InOrOut function to PointF.

---
+++ WeilerAtherton/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WeilerAtherton
+{
+    public static class Extensions
+    {
+        public static DeepPoint.PointStatus InOrOut(this PointF point, PointF[] shape)
+        {
+            return DeepPoint.PointStatus.Undetermined;
+        }
+    }
+}
